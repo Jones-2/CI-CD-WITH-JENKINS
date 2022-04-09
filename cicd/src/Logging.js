7aa6de84718f329265c3886c25c2b872ddd6a72a
Jones-2 <darijones6@gmail.com>
2022-04-09 18:46:33 +0000

    adding a new file

---
+++ cicd/src/Logging.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Logging(){
+    return(
+        <div>
+            <p>Just testing out ci-cd pipelines with Jenkins</p>
+        </div>
+    )
+}
